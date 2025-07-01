947cd9f26736f8d6a5bad1f19865980d294e36cf
sameer2064 <98169594+sameer2064@users.noreply.github.com>
2025-07-01 12:14:03 +0545

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post("/ask", async (req, res) => {
   const userMessage = req.body.message;
 
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     const result = await model.generateContent(userMessage);
     const response = await result.response;
